8691e39b3eb205eda6ff67c65a2849e04713a46f
Karppinen Pasi (EXT) <pasi.karppinen@kone.com>
2021-12-23 16:11:08 +0200

    Module 4 changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -26,7 +26,7 @@ class App extends Component{
     }))
   }
 
-  handleTileClicked(id, color) {
+  handleTileClicked = (id, color) => {
     this.setState((state) => {
 
       const selectedTileIndex = indexOfSelected(this.state.tiles, id, color);
@@ -65,13 +65,21 @@ class App extends Component{
     })
   }
 
+  handleNumTileChange = (num) => {
+    this.setState({
+      numTiles: num,
+      playing: false,
+      tiles: []
+    })
+  } 
+
   render() {
   return (
     <div className="App">
       <header className="App-header">
         Turbo-Matcher
       </header>
-        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame}/>
+        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame} handleNumTileChange={this.handleNumTileChange} />
         <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />      
     </div>
   );
---
+++ src/hooks.js
@@ -0,0 +1,29 @@
+import { useEffect, useRef, useState } from 'react';
+
+const useHover = () => {
+    const ref = useRef();
+    const hovered, setHovered = useState(false);
+
+    useEffect(() => {
+        const refCopy = ref;
+        refCopy.addEventListener('mouseenter', enter);
+        refCopy.addEventListener('mouseleave', leave);        
+
+        return () => {
+            refCopy.current.removeEventListener('mouseenter', enter);
+            refCopy.current.removeEventListener('mouseleave', leave);
+        };
+    });    
+
+    return [ref, hovered];
+}
+
+const enter = () => {
+    setHovered(true);
+}
+
+const leave = () => {
+    setHovered(false);
+}
+
+export default useHover;
