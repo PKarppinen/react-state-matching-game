72750ed61f6fb4862ca7a4a8a99f8586a58896ef
Karppinen Pasi (EXT) <pasi.karppinen@kone.com>
2021-12-23 12:13:12 +0200

    Completed module

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 import './Tile.css'
 
-const Tile = () => {
+const Tile = (props) => {
+
+  const selectedOrMatched = (props.selected || props.matched);
+  const dynamicColor = selectedOrMatched ? {backgroundColor: props.color} : null;
+  let svgComp = null;
+  if (selectedOrMatched) {
+    svgComp = props.svg;
+  }
 
   return (
-    <div className='Tile'>
+    <div className='Tile' style={dynamicColor}>
+      {svgComp}
     </div>
   )
 }
