80f13600dbd8f81e7aa65f5a5bd4bb982ed234d0
Karppinen Pasi (EXT) <pasi.karppinen@kone.com>
2021-12-23 15:01:15 +0200

    Module 3 changes.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-import { createTiles } from '../../misc/utils';
+import { createTiles, indexOfSelected } from '../../misc/utils';
 import './App.css';
 
 class App extends Component{
@@ -22,8 +22,47 @@ class App extends Component{
         playing: true,
         previousTileIndex: null,
         toBeCleared: null,
-        tiles: createTiles(state.numTiles)      
+        tiles: createTiles(state.numTiles, this.handleTileClicked)      
     }))
+  }
+
+  handleTileClicked(id, color) {
+    this.setState((state) => {
+
+      const selectedTileIndex = indexOfSelected(this.state.tiles, id, color);
+      var previousTileIndex = this.state.previousTileIndex;
+      
+      if (this.state.toBeCleared != null) {
+        this.state.tiles[this.state.toBeCleared[0]].selected = false;
+        this.state.tiles[this.state.toBeCleared[1]].selected = false;
+        this.state.toBeCleared = null;
+      }
+
+
+      if (previousTileIndex != null) { 
+        var previousTile = this.state.tiles[previousTileIndex];
+        var selectedTile = this.state.tiles[selectedTileIndex];
+
+        if (previousTile.id != selectedTile.id && previousTile.color == color) {
+          selectedTile.matched = true;
+          previousTile.matched = true;
+          previousTileIndex = null;
+        } else {
+          this.state.toBeCleared = [previousTileIndex, selectedTileIndex];
+          previousTileIndex = null;
+        }
+      } else { 
+        previousTileIndex = selectedTileIndex;
+      }      
+
+      this.state.tiles[selectedTileIndex].selected = true;
+
+      return {
+        tiles: this.state.tiles,
+        toBeCleared: this.state.toBeCleared,
+        previousTileIndex: previousTileIndex
+      }
+    })
   }
 
   render() {
