e27aef4a10098e7fcb8e441862705ec77384e72c
Karppinen Pasi (EXT) <pasi.karppinen@kone.com>
2021-12-27 11:33:42 +0200

    Module 5 fixes

--- refactor/components/Button/Button.js
+++ refactor/components/Button/Button.js
@@ -3,13 +3,14 @@ import './Button.css';
 import GameContext from '../../GameContext.js';
 
 const Button = (props) => (
-    (playing, startGame) => { 
-    return <GameContext.Consumer>
-                <button onClick={startGame}>
-                    {playing ? 'reset' : 'start'}
-                </button>
-            </GameContext.Consumer>;
-    }
+    <GameContext.Consumer>
+        {
+            ({playing, startGame}) => <button onClick={startGame}>
+                {playing ? 'reset' : 'start'}
+            </button>                        
+        }   
+    </GameContext.Consumer>
+    
 )
 
 export default Button
